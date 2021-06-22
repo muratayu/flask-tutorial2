3b061b4feff86bda22560ab794c72a37b37dc7bd
muratayu <toda2020-008@iniad.org>
2021-06-22 05:31:21 +0000

    edit app(dotenv)

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template, request
 import random
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.environ.get("API_KEY")
+print("API_KEY=", API_KEY)
+
+
 app = Flask(__name__)
 
 @app.route('/')
